076710235c58b8361019880e96bfe3b960d24725
Dipesh Acharya <xtranophilist@gmail.com>
2013-05-30 22:45:57 +0545

    added recent posts in front page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
-<div id="front-main">
+
+<div id="primary" class="site-content">
+    <div id="content" role="main">
+
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="entry-content">
+                <?php the_excerpt(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </div><!-- #post-## -->
 
 
 
-    <h3> Recent Posts: </h3>
+    <?php endwhile; // end of the loop. ?>
+</div><!-- #content -->
+</div><!-- #primary -->
 
-    <?php get_archives('postbypost', '100', 'custom', '<li>', '</li>'); ?>
 
-</div>
 <?php get_footer(); ?>
