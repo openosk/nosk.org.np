a0ed29449837f7ff02d16b07bdfa15f5f2c9741d
Dipesh Acharya <xtranophilist@gmail.com>
2013-05-30 23:31:27 +0545

    added author and author_post_links in frontpage

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> <span class="author-info">by <?php the_author_posts_link(); ?> </span>
             <div class="entry-content">
                 <?php the_excerpt(); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
