89752dc1923251a0072a6dd7211607a33067faa8
Dipesh Acharya <xtranophilist@gmail.com>
2013-05-31 14:20:25 +0545

    changed theme name and metadata

--- front-page.php
+++ front-page.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 
+
+<?php
+query_posts('showposts=10');
+?>
+
 <div id="primary" class="site-content front-page">
     <div id="content" role="main">
 
@@ -8,7 +13,7 @@
         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> <span class="author-info">by <?php the_author_posts_link(); ?> </span>
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> <span class="the-author-info">by <?php the_author_posts_link(); ?> </span>
             <div class="entry-content">
                 <?php the_excerpt(); ?>
                 <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
