91929eec2d22eaac9b2b8408eda209bcf769dc55
Dipesh Acharya <xtranophilist@gmail.com>
2013-06-01 08:42:52 +0545

    added page template for live chat

---
+++ page-live-chat.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: IRC Live Chat
+ *
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+    <div id="content" role="main">
+
+        <h1> IRC Live Chat </h1>
+
+        <iframe src="http://webchat.freenode.net?nick=NOSKian.&channels=nosk&prompt=1&uio=d4" width="960" height="400">
+            </iframe>
+
+    </div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
